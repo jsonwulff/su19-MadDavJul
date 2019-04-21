5e5e577d3cd6bb4277dccd8e75939dcb0ec027ae
JSonWulff <julianwulff@gmail.com>
2019-04-21 08:07:49 +0200

    Added LevelParser class and test for it

---
+++ SpaceTaxi/SpaceTaxiTests/TestLevelParser.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace SpaceTaxiTests {
+    
+    [TestFixture]
+    public class TestLevelParser {
+        
+        [Test]
+        public void Test1() {
+            Assert.True(true);
+        }
+    }
+}
