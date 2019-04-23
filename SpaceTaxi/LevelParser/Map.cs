735bb8af624835c774c15d38b1a3b72fffa46e6e
JSonWulff <julianwulff@gmail.com>
2019-04-23 16:28:30 +0200

    Added implemented Map class

---
+++ SpaceTaxi/LevelParser/Map.cs
@@ -0,0 +1,18 @@
+using DIKUArcade.Entities;
+using DIKUArcade.Math;
+
+namespace SpaceTaxi.LevelParser {
+    public class Map {
+        public EntityContainer<Entity> MapContainer;
+        public string LevelName;
+        public Vec2F PlayerPosition;
+        public string CustomerData;
+
+        public Map(EntityContainer<Entity> mapContainer, string levelName, Vec2F playerPosition, string customerData) {
+            MapContainer = mapContainer;
+            LevelName = levelName;
+            PlayerPosition = playerPosition;
+            CustomerData = customerData;
+        }
+    }
+}
