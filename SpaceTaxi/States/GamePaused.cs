6b0ca531583a4dcba6a3a5c3796e1b03d06a25fe
David <david@csillag.com>
2019-05-07 16:48:18 +0200

    Added state classes

---
+++ SpaceTaxi/States/GamePaused.cs
@@ -0,0 +1,110 @@
+using System;
+using System.IO;
+using DIKUArcade.Entities;
+using DIKUArcade.EventBus;
+using DIKUArcade.Graphics;
+using DIKUArcade.Math;
+using DIKUArcade.State;
+
+namespace SpaceTaxi.States {
+    public class GamePaused : IGameState {
+        private static GamePaused instance = null;
+
+        private Entity backGroundImage;
+        private Text[] menuButtons;
+        private Text continueButton;
+        private Text mainMenu;
+        private int activeMenuButton;
+        private int maxMenuButtons = 2;
+        private Vec3I activeColor;
+        private Vec3I inactiveColor;
+
+        public GamePaused() {
+            InitializeGameState();
+        }
+        
+        public static GamePaused GetInstance() {
+            return GamePaused.instance ?? (GamePaused.instance = new GamePaused());
+        }
+
+        public void GameLoop() {
+            throw new System.NotImplementedException();
+        }
+
+        public void InitializeGameState() {
+            activeMenuButton = 0;
+            backGroundImage = new Entity(new StationaryShape(new Vec2F(0,0), new Vec2F(1,1) ), new Image(Path.Combine( "Assets",  "Images", "SpaceBackground.png")));
+            menuButtons = new Text[maxMenuButtons];
+            continueButton = new Text("Continue", new Vec2F(0.5f, 0.5f), new Vec2F(0.2f, 0.2f));
+            mainMenu = new Text("Main Menu", new Vec2F(0.5f, 0.3f), new Vec2F(0.2f, 0.2f));
+            menuButtons[0] = continueButton;
+            menuButtons[1] = mainMenu;
+            
+            activeColor = new Vec3I(255,255,255);
+            inactiveColor = new Vec3I(190,190,190);
+            HandleButtons();
+
+        }
+
+        public void UpdateGameLogic() {
+            GamePaused.GetInstance();
+        }
+
+        public void RenderState() {
+            backGroundImage.RenderEntity();
+            foreach (var button in menuButtons) {
+                button.RenderText();
+            }
+
+        }
+
+        public void HandleButtons() {
+            foreach (var button in menuButtons) {
+                button.SetColor(inactiveColor);                
+            }
+            menuButtons[Math.Abs(activeMenuButton % maxMenuButtons)].SetColor(activeColor);
+            RenderState();
+        }
+
+        public void ActivateButton() {
+            switch (Math.Abs(activeMenuButton % maxMenuButtons)) {
+            case 0:
+                SpaceTaxiBus.GetBus().RegisterEvent(
+                    GameEventFactory<object>.CreateGameEventForAllProcessors(
+                        GameEventType.GameStateEvent, this, "CHANGE_STATE", "GAME_RUNNING", ""));
+                break;
+            case 1:
+                GameRunning.NewInstance();
+                SpaceTaxiBus.GetBus().RegisterEvent(
+                    GameEventFactory<object>.CreateGameEventForAllProcessors(
+                        GameEventType.GameStateEvent, this, "CHANGE_STATE", "MAIN_MENU", ""));
+                break;
+            }
+        }
+        
+        public void KeyPress(string key) {
+            switch (key) {
+            case "KEY_UP":
+                activeMenuButton -= 1;
+                HandleButtons();
+                break;
+            case "KEY_DOWN":
+                activeMenuButton += 1;
+                HandleButtons();
+                break;
+            case "KEY_ENTER":
+                ActivateButton();
+                HandleButtons();
+                break;
+            }
+        }
+        
+        public void HandleKeyEvent(string keyValue, string keyAction) {
+            switch (keyAction) {
+                case "KEY_PRESS":
+                    KeyPress(keyValue);
+                    break;
+            }
+        }
+    }
+}
---
+++ SpaceTaxi/States/MainMenu.cs
@@ -0,0 +1,116 @@
+using System;
+using System.IO;
+using DIKUArcade.Entities;
+using DIKUArcade.EventBus;
+using DIKUArcade.Graphics;
+using DIKUArcade.Math;
+using DIKUArcade.State;
+using SpaceTaxi.States;
+using Image = DIKUArcade.Graphics.Image;
+
+namespace Galaga_Exercise_3.GalagaStates {
+    public class MainMenu : IGameState {
+        private static MainMenu instance = null;
+
+        private Entity backGroundImage;
+        private Text[] menuButtons;
+        private Text newGame;
+        private Text quit;
+        private int activeMenuButton;
+        private int maxMenuButtons = 2;
+        private Vec3I activeColor;
+        private Vec3I inactiveColor;
+
+        public MainMenu() {
+            InitializeGameState();
+        }
+        
+        public static MainMenu GetInstance() {
+            return MainMenu.instance ?? (MainMenu.instance = new MainMenu());
+        }
+
+        public void GameLoop() {
+            throw new System.NotImplementedException();
+        }
+
+        public void InitializeGameState() {
+            
+            /*backGroundImage = new Entity(
+                new StationaryShape(new Vec2F(0,0), new Vec2F(1,1) ), 
+                new Image(Path.Combine( "Assets",  "Images", "TitleImage.png"))); */
+            menuButtons = new Text[maxMenuButtons];
+            activeMenuButton = 0;
+            newGame = new Text("New Game",
+                new Vec2F(0.25f, 0.4f), 
+                new Vec2F(0.5f, 0.5f));
+            quit = new Text(
+                "Quit", new Vec2F(0.25f, 0.2f), 
+                new Vec2F(0.5f, 0.5f));
+            menuButtons[0] = newGame;
+            menuButtons[1] = quit;
+            
+            activeColor = new Vec3I(255,255,255);
+            inactiveColor = new Vec3I(100,100,100);
+            
+            HandleButtons();
+        }
+
+        public void UpdateGameLogic() {
+            MainMenu.GetInstance();
+        }
+
+        public void RenderState() {
+            backGroundImage.RenderEntity();
+            foreach (var button in menuButtons) {
+                button.RenderText();
+            }
+        }
+
+        public void HandleButtons() {
+            foreach (var button in menuButtons) {
+                button.SetColor(inactiveColor);    
+            }
+            menuButtons[Math.Abs(activeMenuButton % maxMenuButtons)].SetColor(activeColor);
+            RenderState();
+        }
+
+        public void ActivateButton() {
+            switch (Math.Abs(activeMenuButton % maxMenuButtons)) {
+            case 0:
+                SpaceTaxiBus.GetBus().RegisterEvent(
+                    GameEventFactory<object>.CreateGameEventForAllProcessors(
+                        GameEventType.GameStateEvent, this, "CHANGE_STATE", "GAME_RUNNING", ""));
+                break;
+            case 1:
+                SpaceTaxiBus.GetBus().RegisterEvent(
+                    GameEventFactory<object>.CreateGameEventForAllProcessors(
+                        GameEventType.WindowEvent, this, "CLOSE_WINDOW", "", ""));
+                break;
+            }
+        }
+
+        public void KeyPress(string key) {
+            switch (key) {
+            case "KEY_UP":
+                activeMenuButton -= 1;
+                HandleButtons();
+                break;
+            case "KEY_DOWN":
+                activeMenuButton += 1;
+                HandleButtons();
+                break;
+            case "KEY_ENTER":
+                ActivateButton();
+                break;
+            }
+        }
+
+        public void HandleKeyEvent(string keyValue, string keyAction) {
+            switch (keyAction) {
+                case "KEY_PRESS":
+                    KeyPress(keyValue);
+                    break;
+            }
+        }
+    }
+}
