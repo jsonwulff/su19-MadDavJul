9b1ebb98501e30197b914e3e9d8f8ab1b10eca6e
JSonWulff <julianwulff@gmail.com>
2019-05-21 22:24:46 +0200

    Added CreateCustomer method

--- SpaceTaxi/Customers/CustomerCreator.cs
+++ SpaceTaxi/Customers/CustomerCreator.cs
@@ -1,5 +1,17 @@
+using System.Collections.Generic;
+
 namespace SpaceTaxi.Customers {
     public class CustomerCreator {
-        
+
+        public List<Customer> CreateCustomers(string[] customerdata) {
+            var retval = new List<Customer>();
+            foreach (var customer in customerdata) {
+                var customerParams = customer.Split(null);
+                retval.Add(new Customer(customerParams[0],customerParams[1],
+                    customerParams[2],customerParams[3],
+                    customerParams[4],customerParams[5]));
+            }
+            return retval;
+        }
     }
 }
