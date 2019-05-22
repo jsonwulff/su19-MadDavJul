b0db59ddab44baccb3298301ba9da5382e5d174a
JSonWulff <julianwulff@gmail.com>
2019-05-22 16:27:21 +0200

    Added ProcessEvent to incoke AddPoints

--- SpaceTaxi/Score.cs
+++ SpaceTaxi/Score.cs
@@ -1,8 +1,10 @@
+using System;
+using DIKUArcade.EventBus;
 using DIKUArcade.Graphics;
 using DIKUArcade.Math;
 
 namespace SpaceTaxi {
-    public class Score {
+    public class Score : IGameEventProcessor<object> {
         private Text display;
         private int score;
 
@@ -10,6 +12,8 @@ namespace SpaceTaxi {
             score = 0;
             display = new Text(score.ToString(), new Vec2F(0.01f, -0.25f), new Vec2F(0.3f, 0.3f));
             display.SetColor(new Vec3I(60, 210, 60));
+            
+            SpaceTaxiBus.GetBus().Subscribe(GameEventType.StatusEvent, this);
         }
         
         public void ResetScore() {
@@ -26,6 +30,17 @@ namespace SpaceTaxi {
         public void RenderScore() {
             display.SetText(string.Format("Score: {0}", score.ToString()));
             display.RenderText();
+        }
+
+        public void ProcessEvent(GameEventType eventType, GameEvent<object> gameEvent) {
+            if (eventType != GameEventType.StatusEvent) {
+                return;
+            }
+            switch (gameEvent.Message) {
+                case "AWARD_POINTS":
+                    AddPoints(Convert.ToInt32(gameEvent.Parameter1));
+                    break;
+            }
         }
     }
 }
