9bb2faa208f226ba8b6375b1cc5cd2099dfe9ee9
JSonWulff <julianwulff@gmail.com>
2019-05-23 12:13:02 +0200

    Added support for constants such as gravity and commonly used Vec2F, but not implemented

---
+++ SpaceTaxi/Constants.cs
@@ -0,0 +1,11 @@
+using System.Linq.Expressions;
+using DIKUArcade.Math;
+
+namespace SpaceTaxi {
+    /// <summary>
+    ///     Constants used in the game.
+    /// </summary>
+    public static class Constants {
+        public static readonly Vec2F VEC2F0 = new Vec2F(0.0f, 0.0f);
+    }
+}
